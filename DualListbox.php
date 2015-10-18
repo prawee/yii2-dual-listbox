a36d81b745c3d292478ba48a26bc3a2b2b4ed6cb
Prawee Wongsa <prawee@hotmail.com>
2015-10-19 04:08:58 +0700

    add DualListbox widget.

---
+++ DualListbox.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @link http://www.yiiassets.com
+ * 10/19/15 AD 2:59 AM
+ * @copyright Copyright (c) 2015 infinitesteam
+ * @author Prawee Wongsa <konkeanweb@gmai.com>
+ * @license BSD-3-Clause
+ */
+namespace prawee\widgets\duallistbox;
+
+use yii\base\Widget;
+
+class DualListbox extends Widget{
+
+}
